cd8e81f0c3077958d1c575f12fdf1af15b57e533
MurtMagomedov <vipsdagestana@mail.ru>
2023-05-16 20:20:24 +0300

    Initial Commit

---
+++ Calculated/CalculatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatedApp.swift
+//  Calculated
+//
+//  Created by Муртазали Магомедов on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
